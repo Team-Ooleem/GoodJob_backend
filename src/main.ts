e1dfeda28d012404a4b5117e5016994fd2e22457
omokgosu <anfakt0606@naver.com>
2025-09-02 00:23:10 +0900

    [ chore ] URL 경로에 무조건 api/ 추가

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    // 모든 요청 경로에 /api prefix 추가
+    app.setGlobalPrefix('api');
+
     await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
