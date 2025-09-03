427cf15c6f518631c70ded84bc1daef78b3f449e
kko3093 <kko3093@gmail.com>
2025-09-04 04:26:33 +0900

    [ feat ] 소셜 로그인 기능 개발 - idx 저장 추가

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,10 +1,9 @@
-// src/auth/auth.module.ts
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
+import { UsersController } from './users.controller';
 import { DatabaseModule } from '../database/database.module';
 
 @Module({
     imports: [DatabaseModule],
-    controllers: [AuthController],
+    controllers: [UsersController],
 })
-export class AuthModule {}
+export class UsersModule {}
