cac39186b603d7728aa8f7312521feb79570c570
Suin Kim <contact@suinkim.me>
2025-09-03 13:17:35 +0900

    feat: 그림 공유 소켓 작성

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 
+import { CollabModule } from './modules/collab/collab.module';
+
 @Module({
     imports: [
         ConfigModule.forRoot({
@@ -11,6 +13,7 @@ import { DatabaseModule } from './database/database.module';
             envFilePath: '.env',
         }),
         DatabaseModule,
+        CollabModule,
     ],
     controllers: [AppController],
     providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,15 @@ import { AppModule } from './app.module';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
+    app.enableCors({
+        origin: 'http://localhost:3001',
+        credentials: true,
+    });
+
     // 모든 요청 경로에 /api prefix 추가
     app.setGlobalPrefix('api');
 
     await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
